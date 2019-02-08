7769d8e9b2e48e4967429bb53dec6796f3733533
Samuel Bauer <bauersam@outlook.com>
2019-02-08 14:55:39 -0700

    finished readme1

--- game.js
+++ game.js
@@ -1,11 +1,25 @@
-var health = 20;
+var health = 100;
+var name = 'Jerry';
+var hits = 0;
 function slap() {
+  hits++;
   health--;
   update();
 }
-
+function punch() {
+  hits++;
+  health = health - 5;
+  update();
+}
+function kick() {
+  hits++;
+  health = health - 10;
+  update();
+}
 function update() {
   document.getElementById('health').innerText = health;
+  document.getElementById('name').innerText = name;
+  document.getElementById('hits').innerText = hits;
 }
 
 update();
