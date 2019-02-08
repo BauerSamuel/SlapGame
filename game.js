88a289359ff3961c3d4eff2bd21f5d3db43e1b1d
Samuel Bauer <bauersam@outlook.com>
2019-02-08 15:14:21 -0700

    object complete

--- game.js
+++ game.js
@@ -1,25 +1,52 @@
-var health = 100;
-var name = 'Jerry';
-var hits = 0;
-function slap() {
-  hits++;
-  health--;
-  update();
-}
-function punch() {
-  hits++;
-  health = health - 5;
-  update();
-}
-function kick() {
-  hits++;
-  health = health - 10;
-  update();
-}
-function update() {
-  document.getElementById('health').innerText = health;
-  document.getElementById('name').innerText = name;
-  document.getElementById('hits').innerText = hits;
+// var health = 100;
+// var name = 'Jerry';
+// var hits = 0;
+
+// var characters = {
+//   name: 'Jerry',
+//   health: 100,
+//   attacks: {
+//     slap: 1,
+//     punch: 5,
+//     kick: 10
+//   }
+// }
+// var characterIndex = {
+//   0: characters.name;
+// }
+
+var jerry = {
+  name: 'Jerry',
+  health: 100,
+  hits: 0,
+  attacks: {
+    slap: 1,
+    punch: 5,
+    kick: 10
+  }
 }
 
-update();
+
+
+function slap(character) {
+  character.hits++;
+  character.health--;
+  update(character);
+}
+function punch(character) {
+  character.hits++;
+  character.health = character.health - 5;
+  update(character);
+}
+function kick(character) {
+  character.hits++;
+  character.health = character.health - 10;
+  update(character);
+}
+function update(character) {
+  document.getElementById('health').innerText = character.health;
+  document.getElementById('name').innerText = character.name;
+  document.getElementById('hits').innerText = character.hits;
+}
+
+update(jerry);
