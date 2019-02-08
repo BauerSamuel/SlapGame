c6f438edd410832dd06460cba73924818505a9ef
Samuel Bauer <bauersam@outlook.com>
2019-02-08 16:07:58 -0700

    working items object/buttons

--- game.js
+++ game.js
@@ -22,38 +22,43 @@ var items = {
     description: 'A critical hit.'
 
   },
-  lightening: {
-    name: 'Lightening',
-    modifier: 4,
-    description: 'It\'s super effective.'
+  armor: {
+    name: 'Armor',
+    modifier: -2,
+    description: 'Protect'
   }
 }
 function slap(character) {
   character.hits++;
-  character.health--;
+  character.health -= (1 + addMods());
   update(character);
 }
 function punch(character) {
   character.hits++;
-  character.health = character.health - 5;
+  character.health -= (5 + addMods());
   update(character);
 }
 function kick(character) {
   character.hits++;
-  character.health = character.health - 10;
+  character.health -= (10 + addMods());
   update(character);
 }
+function giveItem(itemName) {
+  jerry.items.push(items[itemName])
+}
+
 function addMods() {
   let total = 0;
   for (let i = 0; i < jerry.items.length; i++) {
     var item = jerry.items[i];
     total += item.modifier;
   }
+  return total;
 }
-
 
 function update(character) {
   document.getElementById('health').innerText = character.health;
+  console.log('The health is ', character.health)
   document.getElementById('name').innerText = character.name;
   document.getElementById('hits').innerText = character.hits;
 }
